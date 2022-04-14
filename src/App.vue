e5b820b0e1bae50ba7a201e7066e5d7201471140
Dominik Szojda <szojda.98@gmail.com>
2022-04-14 12:29:18 +0200

    poprawki

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,164 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="menu__box">
+    <button @click="prev">Poprzednia strona</button>
+    <button @click="next">Następna strona</button>
+    <button @click="reset">Reset</button>
+    <input @keyup="szukaj" v-model="szukanaFraza" placeholder="szukaj" />
+  </nav>
+
+  <tabela-api
+    :tabToTable="tabToTable"
+    @wyslij-id="wyslij"
+    @sortowanie-tablicy="sortowanie"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TabelaApi from "./components/TabelaApi.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TabelaApi,
+  },
+  data() {
+    return {
+      tab: [],
+      tabToTable: [],
+      limit: 10,
+      szukanaFraza: null,
+      jakieSorotwanie: "id",
+    };
+  },
+  methods: {
+    sortowanie(kolumna) {
+      if (kolumna != this.jakieSorotwanie) {
+        this.jakieSorotwanie = kolumna;
+        this.tabToTable.sort((a, b) => (a[kolumna] > b[kolumna] ? 1 : -1));
+      } else {
+        this.jakieSorotwanie = "";
+        this.tabToTable.sort((a, b) => (a[kolumna] < b[kolumna] ? 1 : -1));
+      }
+    },
+    async wyslij(id) {
+      this.szukanaFraza = null;
+      return fetch(
+        "https://app.linkhouse.co/rekrutacja/api?api_key=APIREKRUTACJA&user_id=" +
+          id,
+        {
+          method: "POST",
+          "content-type": "application/json",
+        }
+      ).then((dana) => console.log(dana));
+    },
+
+    async reset() {
+      this.szukanaFraza = null;
+      this.tabToTable = [];
+      return fetch(
+        "https://app.linkhouse.co/rekrutacja/api?api_key=APIREKRUTACJA",
+        {
+          method: "POST",
+          "content-type": "application/json",
+        }
+      )
+        .then((data) => {
+          if (data.ok) {
+            // console.log(data);
+            return data.json();
+          } else {
+            throw new Error("Something went wrong");
+          }
+        })
+        .then((daneR) => {
+          if (daneR.msg == "ok") {
+            this.tab = daneR.response;
+            let i = 0;
+            for (const item of this.tab) {
+              if (i < 10) {
+                i++;
+                this.tabToTable.push(item);
+              }
+            }
+          } else {
+            throw new Error(daneR.errors);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          throw new Error("Something went wrong");
+        });
+    },
+
+    szukaj() {
+      if (this.szukanaFraza != "") {
+        const TablicaZnalezionych = [];
+        for (const item of this.tabToTable) {
+          if (
+            item.name.includes(this.szukanaFraza) ||
+            item.login.includes(this.szukanaFraza)
+          ) {
+            TablicaZnalezionych.push(item);
+          }
+          this.tabToTable = TablicaZnalezionych;
+        }
+      }
+    },
+    next() {
+      if (this.limit != 100) {
+        this.tabToTable = [];
+        let i = 0;
+        this.limit += 10;
+        for (const item of this.tab) {
+          if (i < this.limit && i > this.limit - 11) {
+            this.tabToTable.push(item);
+          }
+          i++;
+        }
+      }
+    },
+    prev() {
+      if (this.limit != 10) {
+        this.tabToTable = [];
+        let i = 0;
+        this.limit -= 10;
+        for (const item of this.tab) {
+          if (i < this.limit && i > this.limit - 11) {
+            this.tabToTable.push(item);
+          }
+          i++;
+        }
+      }
+    },
+  },
+  async created() {
+    this.reset();
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button {
+  background: inherit;
+  border: none;
+  transition: 0.5s;
+  font-weight: bold;
+  padding: 10px;
+  font-size: 1.2rem;
+}
+button:hover {
+  color: rgb(241, 163, 16);
+  transform: scale(1.2);
+}
+.menu__box {
+  display: flex;
+  justify-content: space-evenly;
+}
+@media screen and (max-width: 600px) {
+  .menu__box {
+    flex-wrap: wrap;
+  }
+  button {
+    font-size: 0.8rem;
+  }
 }
 </style>
